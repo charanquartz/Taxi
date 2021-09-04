9b1212f13081646735f0266db027fe7a3972f209
madhiV <vadhananmadhi@gmail.com>
2021-09-04 17:39:13 +0530

    Added image and some java beans

---
+++ src/CustomerFeedback.java
@@ -0,0 +1,36 @@
+public class CustomerFeedback {
+    private String feedback,Email;
+    private int feedbackNo,ratings;
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getFeedbackNo() {
+        return feedbackNo;
+    }
+
+    public void setFeedbackNo(int feedbackNo) {
+        this.feedbackNo = feedbackNo;
+    }
+
+    public int getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(int ratings) {
+        this.ratings = ratings;
+    }
+}
